ce8ad2aa5978f1cf3b33c82bc796d03e6c3e07f9
Ali Rıza KESKİN <55052890+sulincix@users.noreply.github.com>
2019-11-22 07:17:13 +0000

    Update sulinstrap.sh

--- sulinstrap.sh
+++ sulinstrap.sh
@@ -9,5 +9,5 @@ if [ -d "$sulin" ]
 then
   echo "$sulin alreay exist"
 fi
-inary ar sulin https://downloads.sourceforge.net/project/sulinos/SulinRepository/inary-index.xml -D$sulin
+inary ar sulin https://master.dl.sourceforge.net/project/sulinos/SulinRepository/inary-index.xml -D$sulin
 inary it -c system.base -D$sulin -y --ignore-scom
